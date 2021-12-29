a0517e4ff0fbd2e192bb861bd26093670804df82
Robert Marriott <marriott@telus.net>
2021-12-28 16:35:00 -0800

    Update list of cities

--- pages/formik-dependent-selects.js
+++ pages/formik-dependent-selects.js
@@ -135,14 +135,14 @@ const countries = [
 
 const cities = [
   { id: 1, countryId: 1, name: 'Beijing' },
-  { id: 2, countryId: 1, name: 'Hong Kong' },
+  { id: 2, countryId: 1, name: 'Shanghai' },
   { id: 3, countryId: 2, name: 'Osaka' },
   { id: 4, countryId: 2, name: 'Tokyo' },
   { id: 5, countryId: 3, name: 'London' },
-  { id: 6, countryId: 3, name: 'Newquay' },
-  { id: 7, countryId: 4, name: 'Nice' },
+  { id: 6, countryId: 3, name: 'Manchester' },
+  { id: 7, countryId: 4, name: 'Marseille' },
   { id: 8, countryId: 4, name: 'Paris' },
-  { id: 9, countryId: 5, name: 'Calgary' },
+  { id: 9, countryId: 5, name: 'Toronto' },
   { id: 10, countryId: 5, name: 'Vancouver' },
   { id: 11, countryId: 6, name: 'Los Angeles' },
   { id: 12, countryId: 6, name: 'New York' },
